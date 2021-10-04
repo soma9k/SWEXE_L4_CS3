6a9a6c938f7ee911d437eca1eefbc82a04fded90
SomaKawano <1911140006x@ed.fuk.kindai.ac.jp>
2021-10-04 07:48:52 +0000

    first commit

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,23 @@
+class TopController < ApplicationController
+    def main
+        if session[:login_uid]
+            render 'main'
+        else
+            render 'login'
+        end
+    end
+    
+    def login
+        if params[:uid] == 'kindai' and params[:pass] == 'sanriko'
+            session[:login_uid] = params[:uid]
+            render 'main'
+        else
+            render 'login_failed'
+        end
+    end
+    
+    def logout
+        session.delete(:login_uid)
+        redirect_to root_path
+    end
+end
